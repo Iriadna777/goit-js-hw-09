f2a25be377e083034172e684d7de33095e854136
Iriadna777 <iriadna777@gmail.com>
2024-05-11 22:50:02 +0200

    Update 2-form.js
    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,31 +1,31 @@
-const formData = { email: "", message: "" };
-const form = document.querySelector('.feedback-form');
+let formData = { email: "", message: "" };
+        const form = document.querySelector('.feedback-form');
 
-dokument.addEventListener('DOMContentLoaded', () => {
-    const saveData = localStorage.getItem('feedback-form-state');
-    if (saveData) {
-        formData = JSON.parse(saveData);
-        form.elements.email.value = formData.email;
-        orm.elements.message.value = formData.message;
-    }
-});
+        document.addEventListener('DOMContentLoaded', () => {
+            const savedData = localStorage.getItem('feedback-form-state');
+            if (savedData) {
+                formData = JSON.parse(savedData);
+                form.elements.email.value = formData.email;
+                form.elements.message.value = formData.message;
+            }
+        });
 
-form.addEventListener('input', (event) => {
-    formData[event.target.name] = event.target.value.trim();
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-});
+        form.addEventListener('input', (event) => {
+            formData[event.target.name] = event.target.value.trim();
+            localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+        });
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    if (!formData.email || !formData.message) {
-        alert('Please fill all fields');
-    } else {
-        console.log(formData);
-        localStorage.removeItem('feedback-form-state');
-        formData.email = '';
-        formData.message = '';
-        form.reset();
-    }
-});
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            if (!formData.email || !formData.message) {
+                alert('Please fill all fields');
+            } else {
+                console.log(formData);
+                localStorage.removeItem('feedback-form-state');
+                formData.email = '';
+                formData.message = '';
+                form.reset();
+            }
+        });
       
      
